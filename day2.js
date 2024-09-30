3b117a2504485ac921489349eb0a3cb32fd15476
Srilekha022 <srilekhamasam@gmail.com>
2024-09-30 10:48:05 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,18 @@
+var a=2;
+
+let x=3;
+const y = "srilekha";
+console.log(x);
+console.log(y);
+
+{
+    let b = 10;
+    console.log(a);
+    console.log(b);  
+}
+
+{
+    var c="JS"
+    console.log(c);
+    
+}
